5720c6ced2e4fa34f4aa99c4a2aa25dd8d3b1d15
22Bi13371 <QuanND.22BI13371@usth.edu.vn>
2024-04-12 19:28:43 +0700

    code testing

--- a1_22BI13371/Program.java
+++ a1_22BI13371/Program.java
@@ -1,4 +1,6 @@
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import a1_22BI13371.PostgradStudent;
 import a1_22BI13371.Student;
@@ -6,16 +8,27 @@ import a1_22BI13371.UndergradStudent;
 
 public class Program {
     public static void main(String[] args) {
-        Student student = new Student((int)1000, "student", "1234", "somewhere");
+        Student student = new Student((int)1000, "student a", "1234 a", "somewhere a");
+        Student student2 = new Student((int)1000, "student b", "1234 b ", "somewhere b");
+        Student student3= new Student((int)1000, "student c", "1234 c", "somewhere c");
         UndergradStudent undergradstudent = new UndergradStudent((int)Math.pow(10,6), "undergrad student", "123456", "somewhere 2");
         PostgradStudent postgradstudent = new PostgradStudent((int)Math.pow(10, 8)+100, "postgrad student", "1234567", "somewhere 3", 3f);
 
-        System.out.println(student);
-        System.out.println(undergradstudent);
-        System.out.println(postgradstudent);
+        // System.out.println(student);
+        // System.out.println(undergradstudent);
+        // System.out.println(postgradstudent);
 
-        System.out.println(student.getAddress());
-        student.setAddress(null);
-        System.out.println(student.getAddress());
+        // System.out.println(student.getAddress());
+        // student.setAddress(null);
+        // System.out.println(student.getAddress());
+
+        List<Student> students = new ArrayList<Student>();
+        students.add(student3);
+        students.add(student);
+        students.add(student2);
+        
+        System.out.println(students);
+        Collections.sort(students);
+        System.out.println(students);
     }
 }
